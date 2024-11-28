ef8664820b95e9a5061cb7bcf23a8b223d9ddca5
Fechomap <ferchomap@ejemplo.com>
2024-11-27 19:41:29 -0600

    se realiza modificacion en leyenda

--- index.js
+++ index.js
@@ -508,7 +508,7 @@ async function enviarMensajesConDelay(cajas) {
             
             // Enviar mensaje recordatorio
             await bot.sendMessage(caja.chatId, 
-                "Si cuenta con casetas 🚧, enviame la foto la foto 📸, al PERSONAL para proceder con el registro. Gracias como siempre! ¡Saludos! ✨👋"
+                "Si cuenta con casetas 🚧, enviame la foto 📸, al chat PERSONAL para proceder con el registro. Gracias como siempre! ¡Saludos! ✨👋"
             );
             
             console.log(`✅ Mensajes enviados exitosamente al chat ID: ${caja.chatId}`);
